f63967a964ff5c1a0e5cc7531944f88803c83b51
panz803 <s1411031011@nutc.edu.tw>
2024-07-26 15:35:11 +0800

    CSS外部連結

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -4,13 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <title>HomePage</title>
-    <style>
-        .user-tom button{
-            height: 60px;
-            font-size: medium;
-        }
-    </style>
 </head>
 
 <body>
