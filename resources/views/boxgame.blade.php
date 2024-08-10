325f9ee53d2ba312be67068a487b47aa1d1c4fca
panz803 <s1411031011@nutc.edu.tw>
2024-08-10 11:51:48 +0800

    隨機層數

--- resources/views/boxgame.blade.php
+++ resources/views/boxgame.blade.php
@@ -328,8 +328,14 @@ public class StarPatterns {
         //     changeImg();
         // });
 
+        function layer() {
+            const num = [3, 5, 7]; // 可選的階層數
+            const randomNum = Math.floor(Math.random() * num.length); // 隨機選擇一個索引
+            return num[randomNum]; // 返回隨機選擇的數字
+        }
+
         function triangle1() {
-            let n = 7;
+            let n = layer();
             let result = ""; // 初始化 result
 
             for(let i = 1; i <= n; i++){
@@ -350,7 +356,7 @@ public class StarPatterns {
         }
 
         function triangle2() {
-            let n = 7;
+            let n = layer();
             let result = ""; // 初始化 result
 
             for(let i = 1;i <= n;i++){
@@ -367,7 +373,7 @@ public class StarPatterns {
         }
 
         function triangle3() {
-            let n = 7;
+            let n = layer();
             let result = ""; // 初始化 result
 
             for(let i = n; i >= 1; i--){
@@ -388,7 +394,7 @@ public class StarPatterns {
         }
 
         function triangle4() {
-            let n = 7;
+            let n = layer();
             let result = ""; // 初始化 result
 
             for(let i = n;i >= 1;i--){
